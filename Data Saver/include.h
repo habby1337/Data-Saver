988da1c92eed333d3c9c52c1dcd12d1afeeecc10
Federico Tensi <federicotensi@gmail.com>
2018-09-19 12:28:54 +0200

    added some libraries

--- Data Saver/include.h
+++ Data Saver/include.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#define CURL_STATICLIB
+
 #include <iostream>
 #include <string>
 #include <Windows.h>
 #include <limits>
 #include <locale>
 #include <fstream>
+#include <curl/curl.h>
